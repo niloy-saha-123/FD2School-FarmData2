d292cc504d1e29201ff02a935267d55b531dab8b
Niloy Saha <niloysaha2404@gmail.com>
2024-10-30 11:59:57 -0400

    Added test for crop dropdown with checks for first, fifth and last options

--- farmdata2/farmdata2_modules/fd2_school/cypress/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/cypress/e2e.defaults.spec.js
@@ -5,7 +5,19 @@ describe("Test the harvest report default values", () => {
   })
 
   it("Check the page header", () => {
-    cy.get("[data-cy=page-header]")
-    .should("have.text", "Harvest Report")
+    cy.get('[data-cy=page-header]')
+      .should('have.text', 'Harvest Report')
+  })
+
+  it("Check the default dates", () => {
+    cy.get('[data-cy=start-date]')
+      .should('have.value', '2020-05-05')
+    
+    cy.get('[data-cy=end-date]')
+      .should('have.value', '2020-05-15')
+  })
+
+  it("Check the crop dropdown", () => {
+    cy.get('[data-cy=crop-dropdown]')
   })
 })
