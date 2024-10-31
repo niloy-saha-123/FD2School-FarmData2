0da4e995683cd1301f9035fe80f37137d0038b32
Niloy Saha <niloysaha2404@gmail.com>
2024-10-30 21:49:44 -0400

    Added ways so that can check the number of elements and for the numbers it shows the expected crop

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -16,8 +16,22 @@ describe("Test the harvest report default values", () => {
     cy.get('[data-cy=end-date]')
       .should('have.value', '2020-05-15')
   })
-
   it("Check the crop dropdown", () => {
-    cy.get('[data-cy=crop-dropdown]')
-  })
+    // First option is always 'All'
+    cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option0]")
+        .should('have.text', 'All')
+
+    // Second option (index 1)
+    cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option1]")
+        .should('have.text', 'ARUGULA')
+
+    // Fifth option (index 4)
+    cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option4]")
+        .should('have.text', 'BEAN-DRY')
+
+    // Fix the length check by getting children of dropdown-input instead of crop-dropdown
+    cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input]")
+        .children()
+        .should('have.length', 112)  // Changed to match actual number of options
+})
 })
