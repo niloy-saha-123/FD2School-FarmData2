c29c332bef1c9b6859ed04caa64ce532b744327b
Niloy Saha <niloysaha2404@gmail.com>
2024-10-31 17:18:43 -0400

    Added new Cypress test file for harvest report table

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,10 @@
+describe("Test the harvest report table", () => {
+  beforeEach(() => {
+      cy.login("manager1", "farmdata2")
+      cy.visit("/farm/fd2-school/fd2")
+  })
+
+  it("Test placeholder", () => {
+      // Empty test to start with
+  })
+})
