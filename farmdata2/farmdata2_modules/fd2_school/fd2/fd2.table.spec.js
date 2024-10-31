7b0ed39f0fc1271b41ba3e8b2fc88de7d2502425
Niloy Saha <niloysaha2404@gmail.com>
2024-10-31 17:23:29 -0400

    Added test to verify harvest table headers

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -4,7 +4,15 @@ describe("Test the harvest report table", () => {
       cy.visit("/farm/fd2-school/fd2")
   })
 
-  it("Test placeholder", () => {
-      // Empty test to start with
+  it("Check table headers", () => {
+    // Click generate report to make table appear
+    cy.get('[data-cy=generate-report-button]').click()
+
+    // Check each header
+    cy.get("[data-cy=h0]").should("have.text", "Date")
+    cy.get("[data-cy=h1]").should("have.text", "Area")
+    cy.get("[data-cy=h2]").should("have.text", "Crop")
+    cy.get("[data-cy=h3]").should("have.text", "Yield")
+    cy.get("[data-cy=h4]").should("have.text", "Units")
   })
 })
