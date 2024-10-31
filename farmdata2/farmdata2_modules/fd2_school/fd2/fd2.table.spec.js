8cec39eb8c3bfc88c7c59430a158ebb34a8a173d
Niloy Saha <niloysaha2404@gmail.com>
2024-10-31 17:33:00 -0400

    Added test to verify number of columns in harvest table

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -5,14 +5,20 @@ describe("Test the harvest report table", () => {
   })
 
   it("Check table headers", () => {
-    // Click generate report to make table appear
-    cy.get('[data-cy=generate-report-button]').click()
+      // Click generate report button
+      cy.get('[data-cy=generate-report-button]').click()
 
-    // Check each header
-    cy.get("[data-cy=h0]").should("have.text", "Date")
-    cy.get("[data-cy=h1]").should("have.text", "Area")
-    cy.get("[data-cy=h2]").should("have.text", "Crop")
-    cy.get("[data-cy=h3]").should("have.text", "Yield")
-    cy.get("[data-cy=h4]").should("have.text", "Units")
+      // Check each header
+      cy.get("[data-cy=h0]").should("have.text", "Date")
+      cy.get("[data-cy=h1]").should("have.text", "Area")
+      cy.get("[data-cy=h2]").should("have.text", "Crop")
+      cy.get("[data-cy=h3]").should("have.text", "Yield")
+      cy.get("[data-cy=h4]").should("have.text", "Units")
+
+      // Check number of columns using the row containing headers
+      cy.get("[data-cy=h0]")
+          .parent()
+          .children()
+          .should("have.length", 6)
   })
 })
