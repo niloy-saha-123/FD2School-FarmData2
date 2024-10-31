d2193c2b5af10b20987332d2ef4269f456b4c271
Niloy Saha <niloysaha2404@gmail.com>
2024-10-30 21:31:13 -0400

    Updated defaults spec file to use FD2 tab instead of E2E tab

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -1,7 +1,7 @@
 describe("Test the harvest report default values", () => {
   beforeEach(() => {
     cy.login("manager1", "farmdata2")
-    cy.visit("/farm/fd2-school/e2e")
+    cy.visit("/farm/fd2-school/fd2")
   })
 
   it("Check the page header", () => {
